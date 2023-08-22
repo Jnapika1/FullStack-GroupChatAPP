ebc25b972201fdf149c6232b9dcc53e17f8333ab
Jnapika1 <jnapika.aish@gmail.com>
2023-08-22 14:35:39 +0530

    added sockets

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 const userRoutes = require('./routers/userRoutes');
 const cors = require('cors');
+const socketIo = require('socket.io');
+
 
 const User = require('./models/userdetails');
 const Chats = require('./models/chats');
@@ -11,6 +14,9 @@ const Group = require('./models/groups');
 const UserGroup = require('./models/usergroup');
 
 const app = express();
+
+const server = http.createServer(app); // Create an http server
+const io = socketIo(server);
 
 app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views')));
@@ -33,11 +39,38 @@ User.hasMany(UserGroup);
 Group.belongsToMany(User, {through: UserGroup});
 Group.hasMany(UserGroup);
 
+const initializeSocket = require('./sockets/socket');
+initializeSocket(io);
+
 sequelize.sync()
-.then(result=>{
+.then(result=>{ 
     // console.log(result);
-    app.listen(3000);
+    server.listen(3000);
+    // const server = app.listen(3000);
+    // const io=socketIo(server);
+    // const groupNamespace = io.of('/group');
+
+    // io.on('connection', (socket) => {
+    //     // console.log('A user connected', socket.id);
+    //     socket.on('joinGroup', data=>{
+    //         // console.log('group joined', data);
+    //         socket.join(data);
+    //         // console.log(socket.rooms);
+    //     })
+    //     // console.log(socket.rooms);
+    //     socket.on('newMessage', data=>{
+    //         io.to(JSON.parse(data.groupid)).emit('message', data.message);
+    //         // console.log('sending message to users', data.groupid, data.message);
+    //     })
+    //     // Handle events on the socket
+    //     socket.on('disconnect', () => {
+    //       console.log('A user disconnected');
+    //     });
+    // });
+    
     
 }).catch(err=>{
     console.log(err);
 })
+
+// module.exports={io};
---
+++ sockets/socket.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userdetails');
+// const Chat = require('../models/chats');
+require('dotenv').config();
+
+module.exports = function initializeSocket(io) {
+io.on('connection', (socket) => {
+    // console.log('A user connected', socket.id);
+    socket.on('joinGroup', data=>{
+        // console.log('group joined', data);
+        socket.join(data);
+        // console.log(socket.rooms);
+    })
+    // console.log(socket.rooms);
+    socket.on('newMessage', async(data)=>{
+
+        const user = jwt.verify(data.token, process.env.TOKEN_KEY);
+        const userFound = await User.findByPk(user.userId)
+        // console.log(userFound.name);
+        const username=userFound.name;
+
+        io.to(JSON.parse(data.groupid)).emit('message', {username: username, message:data.message, groupId: data.groupid});
+        // console.log('sending message to users', data.groupid, data.message);
+    })
+    // Handle events on the socket
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+});
+}
+
